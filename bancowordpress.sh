17c9429b160ad3193701e01da891ba95c0fb0bb2
Helter Ribeiro <39557564+HelterL@users.noreply.github.com>
2019-06-12 20:26:59 -0300

    Update bancowordpress.sh

--- bancowordpress.sh
+++ bancowordpress.sh
@@ -5,8 +5,8 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/ password wordpress'
 sudo mysql <<EOF
 CREATE DATABASE wordpress;
 CREATE USER 'wordpress'@'$IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress';
-GRANT ALL PRIVILEGES on wordpress.* TO 'wordpress'@'IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress';
-GRANT ALL PRIVILEGES on *.* TO 'root'@'IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress';
+GRANT ALL PRIVILEGES on wordpress.* TO 'wordpress'@'$IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress';
+GRANT ALL PRIVILEGES on *.* TO 'root'@'$IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress';
 FLUSH PRIVILEGES;
 \q;
 EOF
