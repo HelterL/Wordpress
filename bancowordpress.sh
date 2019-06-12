cd15d415148ac1079bb57ecf51aa8840a3502dc6
Helter Ribeiro <39557564+HelterL@users.noreply.github.com>
2019-06-12 19:04:59 -0300

    Add files via upload

--- bancowordpress.sh
+++ bancowordpress.sh
@@ -1,8 +1,11 @@
 sudo apt-get install mysql-client mysql-server -y
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo systemctl restart mysql
 sudo debconf-set-selections <<< 'mysql-server mysql-server/ password wordpress'
 sudo mysql <<EOF
 CREATE DATABASE wordpress;
-GRANT ALL PRIVILEGES on wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
+CREATE USER 'wordpress'@'$IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress';
+GRANT ALL PRIVILEGES on wordpress.* TO 'wordpress'@'IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress';
 FLUSH PRIVILEGES;
 \q;
 EOF
