1878518760784ad778758ea54c69c717ed12fcb8
Helter Ribeiro <39557564+HelterL@users.noreply.github.com>
2019-06-13 00:00:33 -0300

    Update bancowordpress.sh

--- bancowordpress.sh
+++ bancowordpress.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
-sudo apt-get install mysql-client mysql-server -y
-sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo systemctl restart mysql
-sudo debconf-set-selections <<< 'mysql-server mysql-server/ password wordpress'
+#!/bin/bash 
+#IP_PRIVATE=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+#Criando o Banco de Dados do WordPress
+sudo apt -y update
+sudo apt-get -y install mysql-server
+sudo sed -i '43s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+#sudo sed -i '43s/^/#/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo service mysql restart
 sudo mysql <<EOF
 CREATE DATABASE wordpress;
-CREATE USER 'wordpress'@'$IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress';
-GRANT ALL ON wordpress.* TO 'wordpress'@'$IP_PRIVATE_WEB';
-GRANT ALL ON *.* TO 'root'@'$IP_PRIVATE_WEB' IDENTIFIED BY 'wordpress' WITH GRANT OPTION;
+CREATE USER `wp_admin`@`$IP_PRIVATE_WEB` IDENTIFIED BY 'root';
+GRANT ALL ON wordpress.* TO `wp_admin`@`$IP_PRIVATE_WEB`;
+GRANT ALL ON *.* TO 'root'@'$IP_PRIVATE_WEB' IDENTIFIED BY 'root' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-\q;
 EOF
-sudo systemctl restart mysql
