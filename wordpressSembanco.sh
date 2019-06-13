bad5390898bf314d72034ad65ddfefde53550e68
Helter Ribeiro <39557564+HelterL@users.noreply.github.com>
2019-06-12 23:29:12 -0300

    Update wordpressSembanco.sh

--- wordpressSembanco.sh
+++ wordpressSembanco.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-echo "Instalacao wordpress em andamento, aguarde..."
-sudo apt-get update
-sudo apt-get install apache2 apache2-utils -y
-sudo systemctl enable apache2
-sudo systemctl start apache2
+IP_PUBLIC=`curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
+echo "Instalação wordpress em andamento, aguarde..."
+sudo apt-get update 
+sudo apt-get install apache2 apache2-utils -y 
+sudo systemctl enable apache2 
+sudo systemctl start apache2  
 sudo apt-get install php7.2 php7.2-mysql libapache2-mod-php7.2 php7.2-cli php7.2-cgi php7.2-gd -y
 cd /tmp
 wget -c http://wordpress.org/latest.tar.gz
@@ -17,10 +17,14 @@ sudo sed -i 's/username_here/wordpress/g' wp-config.php
 sudo sed -i 's/password_here/wordpress/g' wp-config.php
 sudo systemctl restart apache2.service
 sudo rm -rf /var/www/html/index.html
-sudo sed -i '/warn/a <Directory /var/www/html/>\n   AllowOverride All\n</Directory>' /etc/apache2/sites-available/000-default.conf
-sudo a2enmod rewrite
+sudo sed -i '10s/^/        ServerName '$IP_PUBLIC'/' /etc/apache2/sites-available/000-default.conf
+sudo sed -i '12s/html/wordpress/' /etc/apache2/sites-available/000-default.conf
+sudo sed -i '13s/^/        ServerAlias www.hlrp.com/' /etc/apache2/sites-available/000-default.conf
+sudo a2ensite wordpress.conf
 sudo service apache2 restart
 sudo touch /var/www/html/.htaccess
 sudo chown :www-data /var/www/html/.htaccess
 sudo chmod 664 /var/www/html/.htaccess
 sudo service apache2 restart
+echo "No browser coloque seu ip publico"
+echo -e "Usuário: wordpress\nSenha: wordpress\n"
