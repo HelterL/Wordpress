c7a56a96d150a838eb5d838f91923086aade765b
Helter Ribeiro <39557564+HelterL@users.noreply.github.com>
2019-06-14 10:58:56 -0300

    Update wordpressSembanco.sh

--- wordpressSembanco.sh
+++ wordpressSembanco.sh
@@ -1,26 +1,34 @@
-echo "Instalação wordpress em andamento, aguarde..."
-sudo apt-get update 
-sudo apt-get install apache2 apache2-utils -y 
-sudo systemctl enable apache2 
-sudo systemctl start apache2  
-sudo apt-get install mysql-client -y
-sudo apt-get install php7.2 php7.2-mysql libapache2-mod-php7.2 php7.2-cli php7.2-cgi php7.2-gd -y
+
+apt-get update 
+apt-get install apache2 apache2-utils -y 
+systemctl enable apache2 
+systemctl start apache2  
+apt-get install mysql-client -y
+apt-get install php7.2 php7.2-mysql libapache2-mod-php7.2 php7.2-cli php7.2-cgi php7.2-gd -y
 cd /tmp
 wget -c http://wordpress.org/latest.tar.gz
-sudo tar -xzvf latest.tar.gz
-sudo rsync -av wordpress/* /var/www/html/
-sudo chmod -R 755 /var/www/html/
+tar -xzvf latest.tar.gz
+rsync -av wordpress/* /var/www/html/
+chmod -R 755 /var/www/html/
 cd /var/www/html
-sudo mv wp-config-sample.php wp-config.php
-sudo sed -i 's/database_name_here/wordpress/g' wp-config.php
-sudo sed -i 's/username_here/wordpress/g' wp-config.php
-sudo sed -i 's/password_here/wordpress/g' wp-config.php
-sudo systemctl restart apache2.service
-sudo rm -rf /var/www/html/index.html
-sudo sed -i '/warn/a <Directory /var/www/html/>\n   AllowOverride All\n</Directory>' /etc/apache2/sites-available/000-default.conf
-sudo a2enmod rewrite
-sudo service apache2 restart
-sudo touch /var/www/html/.htaccess
-sudo chown :www-data /var/www/html/.htaccess
-sudo chmod 664 /var/www/html/.htaccess
-sudo service apache2 restart
+mv wp-config-sample.php wp-config.php
+
+#passando dados de usuario e ip remoto para acesso do BD
+
+sed -i 's/database_name_here/wordpress/g' wp-config.php
+sed -i 's/username_here/wordpress/g' wp-config.php
+sed -i 's/password_here/wordpress/g' wp-config.php
+sed -i "s/localhost/$IP_PRIVATE_BD/g" wp-config.php
+
+systemctl restart apache2.service
+
+rm -rf /var/www/html/index.html
+sed -i '/warn/a <Directory /var/www/html/>\n   AllowOverride All\n</Directory>' /etc/apache2/sites-available/000-default.conf
+
+a2enmod rewrite
+service apache2 restart
+
+touch /var/www/html/.htaccess
+chown :www-data /var/www/html/.htaccess
+chmod 664 /var/www/html/.htaccess
+service apache2 restart
