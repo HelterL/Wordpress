ef5f30121aa87aa0f9c4ea94a6daaa7299d8af6e
Helter Ribeiro <39557564+HelterL@users.noreply.github.com>
2019-06-24 13:02:01 -0300

    Update WordpressWPcliSemBanco.sh

--- WordpressWPcliSemBanco.sh
+++ WordpressWPcliSemBanco.sh
@@ -11,7 +11,7 @@ chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 sudo -u ubuntu -i -- wp core download --locale=pt_BR
 sudo -u ubuntu -i -- wp core config --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=$IPBANCOPRIVADO --dbprefix=word
-sudo -u ubuntu -i -- wp core install --url="http://$IP_Public" --title="Topicos Avancados" --admin_user="$USUARIOWORDPRESS" --admin_password="$SENHAWORDPRESS" --admin_email="topicos@gmail.com"
+sudo -u ubuntu -i -- wp core install --url="http://$IP_Public" --title="Topicos Avancados" --admin_user=$USUARIOWORDPRESS --admin_password=$SENHAWORDPRESS --admin_email="topicos@gmail.com"
 sudo mv * /var/www/html
 sudo rm -fr /var/www/html/index.html
 sudo systemctl restart apache2
